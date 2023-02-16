32951b965f396c8015726b6f14a622f67900e450
Boris Korobeinikov <Boris_Korobeinikov@epam.com>
2023-02-16 12:12:09 +0600

    AdoDemoApp created

---
+++ Data Access/AdoDemoApp/AdoDemoApp/DbInitializer.cs
@@ -0,0 +1,38 @@
+﻿using System.Data.SqlClient;
+namespace DemoConsoleApp;
+
+internal class DbInitializer
+{
+    private readonly string connectionString;
+
+    public DbInitializer(string connectionString)
+    {
+        this.connectionString = connectionString;
+    }
+
+    internal void CreateTables()
+    {
+        string query =
+            "CREATE TABLE Student(" +
+            "Id INT IDENTITY(1,1) PRIMARY KEY," +
+            "FirstName NVARCHAR(100)," +
+            "LastName NVARCHAR(100)" +
+            ");";
+
+        using (SqlConnection connection = new(connectionString))
+        {
+            SqlCommand command = new(query, connection);
+
+            try
+            {
+                connection.Open();
+                command.ExecuteNonQuery();
+                Console.WriteLine("Table created");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERROR: "+ ex.Message);
+            }
+        }; 
+    }
+}
---
+++ Data Access/AdoDemoApp/AdoDemoApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using DemoConsoleApp;
+using Microsoft.Extensions.Configuration;
+
+IConfiguration config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+string connectionString = config.GetConnectionString("DbConnection") ?? throw new InvalidOperationException("Unable to load connection string");
+
+var dbInitializer = new DbInitializer(connectionString);
+
+dbInitializer.CreateTables();
+
+Console.WriteLine(connectionString);
